52c0d41604f07202056d57ff694bbc57bf06f0a0
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-08-18 14:06:39 +0300

    Create Planet.java

---
+++ src/main/java/org/example/entities/Planet.java
@@ -0,0 +1,21 @@
+package org.example.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "planet")
+public class Planet {
+    @Id
+    private String id;
+    @Column
+    private String name;
+}
