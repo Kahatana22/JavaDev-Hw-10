569d430888f5e2ccdf7875fb5afd124b49443ac7
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-08-18 14:06:32 +0300

    Create Client.java

---
+++ src/main/java/org/example/entities/Client.java
@@ -0,0 +1,22 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "client")
+@Data
+@NoArgsConstructor
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+
+    public Client(String name) {
+        this.name = name;
+    }
+}
