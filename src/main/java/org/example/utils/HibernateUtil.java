364f651a0c3f77e16134da83fa18506b01b0836a
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-08-19 11:55:11 +0300

    Update HibernateUtil.java

--- src/main/java/org/example/utils/HibernateUtil.java
+++ src/main/java/org/example/utils/HibernateUtil.java
@@ -3,7 +3,6 @@ package org.example.utils;
 import lombok.Getter;
 import org.example.entities.Client;
 import org.example.entities.Planet;
-import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -21,14 +20,9 @@ public class HibernateUtil {
                 .addAnnotatedClass(Client.class)
                 .addAnnotatedClass(Planet.class)
                 .buildSessionFactory();
-        Flyway.configure().dataSource("jdbc:h2:./homework1", null, null).load().migrate();
     }
 
     public static HibernateUtil getInstance() {
         return INSTANCE;
-    }
-
-    public void close() {
-        sessionFactory.close();
     }
 }
