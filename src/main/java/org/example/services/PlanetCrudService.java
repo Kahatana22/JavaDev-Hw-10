d41c9c17bb2288119743965c9fa605d89c8ea7a8
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-08-19 11:55:00 +0300

    Update PlanetCrudService.java

--- src/main/java/org/example/services/PlanetCrudService.java
+++ src/main/java/org/example/services/PlanetCrudService.java
@@ -9,42 +9,51 @@ import java.util.List;
 import java.util.Objects;
 
 public class PlanetCrudService {
-    private Session session;
-
-    public PlanetCrudService() {
-        session = HibernateUtil.getInstance().getSessionFactory().openSession();
-    }
 
     public void createPlanet(Planet planet) {
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         session.persist(planet);
         transaction.commit();
+        session.close();
     }
 
     public Planet readPlanet(String id) {
-        return session.get(Planet.class, id);
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
+        Planet planet = session.get(Planet.class, id);
+        session.close();
+        return planet;
     }
 
     public void updatePlanet(String id, String name) {
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         session.createQuery("UPDATE Planet SET name =: name WHERE id =: planet_id")
                 .setParameter("name", name)
                 .setParameter("planet_id", id)
                 .executeUpdate();
         transaction.commit();
+        session.close();
     }
 
     public void deletePlanet(String id) {
         Planet planet = readPlanet(id);
 
         if (Objects.nonNull(planet)) {
+            Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
             Transaction transaction = session.beginTransaction();
             session.remove(planet);
             transaction.commit();
+            session.close();
         }
     }
 
     public List<Planet> getAllPlanets() {
-        return session.createQuery("select planet from Planet planet", Planet.class).list();
+        List<Planet> planets;
+
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
+        planets = session.createQuery("select planet from Planet planet", Planet.class).list();
+        session.close();
+        return planets;
     }
 }
