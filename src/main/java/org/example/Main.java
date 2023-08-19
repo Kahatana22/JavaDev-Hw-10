32e2e22d4807a44cfdeb478cbd832152c15b2fc1
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-08-19 11:53:49 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,18 @@ import org.example.entities.Client;
 import org.example.entities.Planet;
 import org.example.services.ClientCrudService;
 import org.example.services.PlanetCrudService;
+import org.example.utils.MigrationUtil;
 
 public class Main {
     public static void main(String[] args) {
+        MigrationUtil migrationUtil = new MigrationUtil();
+        migrationUtil.startMigration();
+
         ClientCrudService clientCrudService = new ClientCrudService();
         clientCrudService.createClient(new Client("Petro"));
         System.out.println(clientCrudService.readClient(2L));
         clientCrudService.updateClient(14L, "Vlad");
-        clientCrudService.deleteClient(17L);
+        clientCrudService.deleteClient(18L);
         clientCrudService.getAllClients().forEach(System.out::println);
 
         PlanetCrudService planetCrudService = new PlanetCrudService();
