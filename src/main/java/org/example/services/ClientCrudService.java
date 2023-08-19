9819d513173f55db0b60731ada158422d841c345
Kahatana22 <120562156+Kahatana22@users.noreply.github.com>
2023-08-19 11:54:50 +0300

    Update ClientCrudService.java

--- src/main/java/org/example/services/ClientCrudService.java
+++ src/main/java/org/example/services/ClientCrudService.java
@@ -9,42 +9,51 @@ import java.util.List;
 import java.util.Objects;
 
 public class ClientCrudService {
-    private Session session;
-
-    public ClientCrudService() {
-        session = HibernateUtil.getInstance().getSessionFactory().openSession();
-    }
 
     public void createClient(Client client) {
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         session.persist(client);
         transaction.commit();
+        session.close();
     }
 
     public Client readClient(Long id) {
-        return session.get(Client.class, id);
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
+        Client client = session.get(Client.class, id);
+        session.close();
+        return client;
     }
 
     public void updateClient(Long id, String name) {
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
         session.createQuery("UPDATE Client SET name =: name WHERE id =: client_id")
                 .setParameter("name", name)
                 .setParameter("client_id", id)
                 .executeUpdate();
         transaction.commit();
+        session.close();
     }
 
     public void deleteClient(Long id) {
         Client client = readClient(id);
 
         if (Objects.nonNull(client)) {
+            Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
             Transaction transaction = session.beginTransaction();
             session.remove(client);
             transaction.commit();
+            session.close();
         }
     }
 
     public List<Client> getAllClients() {
-        return session.createQuery("select client from Client client", Client.class).list();
+        List<Client> clients;
+
+        Session session = HibernateUtil.getInstance().getSessionFactory().openSession();
+        clients = session.createQuery("select client from Client client", Client.class).list();
+        session.close();
+        return clients;
     }
 }
